c906ff0d5ef52dde16a7ba6ef7a10ae492d740f1
Dmitry <teramisu01@gmail.com>
2020-03-14 16:04:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(arguments.length === 0) {
+        return 0;
+    };
+    if(array.length === 0){
+        return 0;
+    };
+    let min = array[0];
+    for(let i = 1; i < array.length; i++) {
+        if(min > array[i]){
+            min = array[i];
+        };
+    };
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(arguments.length === 0) {
+        return 0;
+    };
+    if(array.length === 0){
+        return 0;
+    };
+    let max = array[0];
+    for(let i = 1; i < array.length; i++) {
+        if(max < array[i]){
+            max = array[i];
+        };
+    };
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(arguments.length === 0) {
+        return 0;
+    };
+    if(array.length === 0){
+        return 0;
+    };
+    let summ = array[0];
+    for(let i = 1; i < array.length; i++){
+        summ += array[i];
+    };
+  return summ / array.length;
+};
